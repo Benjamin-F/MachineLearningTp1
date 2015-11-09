10b63894030a2097e61a660c65e08eff09fee9d7
Benjamin-F <b.fanjeaux@gmail.com>
2015-11-09 21:00:02 +0100

    Norm and random

--- src/main.m
+++ src/main.m
@@ -15,15 +15,31 @@ xtest = tmp;
 
 Ya=change_Y_discri(yapp);
 Yt=change_Y_discri(ytest);
-%Add normalisation functions
 
+%normalisation 
+xappmax=max(max(xapp));
+xtestmax=max(max(xtest));
+xapp = xapp/2;
+xtest = xtest/2;
+
+%Randomization
 [xapp, yapp, Ya] = shakeTable(xapp, yapp, Ya);
+
+%Pixel par pixel
+RES = [];
+
+for idx = 1:numel(xapp)
+    element = xapp(idx);
+    [TEA, TET, pmc] = apprend_pmc (xapp(:,element),Ya,xtest,Yt,0.01,{10,10});
+    res = test (xapp(:,element)
+    RES = [RES; res];
+end
 
 % Learning
 %Taux Erreur Apprentissage
 %Taux Erreur en Test
 %(10,10) 2 layers of 10 neurones eacb
-[TEA, TET, pmc]=apprend_pmc(xapp,Ya,xtest,Yt,0.01,{10,10});
+%[TEA, TET, pmc]=apprend_pmc(xapp,Ya,xtest,Yt,0.01,{10,10});
 
 %Taux de succès
 SuccessRateApp = test_classif_pmc(xapp,Ya,pmc);
